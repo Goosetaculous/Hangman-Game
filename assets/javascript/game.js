8a1bb208443a5ef45c2d3f9d3486833359458015
Goose <josephtrop@gmail.com>
2017-04-27 00:03:41 -0700

    stopping point

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,62 +17,90 @@
      hints  : [" Vodoo "," smack "," God "," Navy theme song "]
  }
 
- var ratm={
-     name   : "Rage against the machine",
-     video  : "https://www.youtube.com/watch?v=bWXazVhlyxQ&list=PL2EbESdPFdo5Xwx2SHw0_SsayQzOqZaNZ",
-     hints  : [" Political band ", " Killing in the name of "," Tom Morello "," Zack Dela Roca "]
+ var incubus={
+     name   : "Incubus",
+     video  : '<iframe width="400" height="200" src="http://www.youtube.com/embed/fgT9zGkiLig?autoplay=1" frameborder="0" allowfullscreen></iframe>',
+     hints  : [" Pardon Me ", " Drive "," Make Yourself "," Brand Boyd "]
  }
 
  var bands =[
-     ratm, godsmack, korn
+     incubus, godsmack, korn
  ]
 
 
  /*******
   *
-  * @param rand -  random pic band to be guessed
+  * @param rand -  random pic band to be guessed.  Used for the hints
   * @param arrAnswer - array of blanks with the length of the band name
   *
   *******/
- function startGame(rand, arrAnswer){
+ function startGame(rand, winArray, arrBlank){
      var triesCtr=10
      var alphaExp=/^[0-9a-zA-Z]+$/;
-     var guessedLetter = document.getElementById("guess-letter").innerHTML
      var arrUserGuess=[]
      var score=0
-     alert(guessedLetter)
+     var guessedLetter
+
 
      document.getElementById("hints").innerHTML= "<h3>HINTS</h3> "+ bands[rand].hints
      document.getElementById("instructions").innerHTML = ""
+     document.getElementById("tries").innerHTML=triesCtr + " tries Remaining"
+
+
      document.onkeyup=function(evt){
-         if(evt.key && checkLetter( arrAnswer , evt.key)  ){
 
-             arrUserGuess.push( evt.key ) //create an array guessed letters
+         if(evt.key && checkLetter( winArray , evt.key)  ){
 
-
-             //get the character
-             //find the index(es) of where it exist
-             console.log( findArrayIndexs(evt.key, arrAnswer) )
-             //replace the guessed
-
-             
-
-
-
-
-
-
+             guessedLetter = replaceArray(arrBlank,findArrayIndexs(evt.key, winArray), evt.key).toString()
+             guessedLetter = guessedLetter.replace(",","")
+             document.getElementById("guess-letter").innerHTML= guessedLetter
 
          }else if( evt.key.length == 1 && evt.key.length != " " && alphaExp.test(evt.key)   ) {
+             arrUserGuess.push( evt.key ) //create an array guessed letters
              triesCtr--
-             document.getElementById("tries").innerHTML=triesCtr + "tries left"
+             document.getElementById("tries").innerHTML=triesCtr + " tries Remaining"
+             alreadyGuessed(arrUserGuess,evt.key)
+         }
+
+         if(arrBlank.indexOf('_') < 0){
+             document.getElementById("tries").innerHTML=""
+             document.getElementById("game-result").innerHTML=bands[rand].video
+             document.getElementById("guess-letter").innerHTML="<h1>GOOD JOB!</h1>"
+
          }
          if(triesCtr == 0){
              document.getElementById("tries").innerHTML=""
              document.getElementById("game-result").innerHTML=lost.video
+             document.getElementById("guess-letter").innerHTML="<h1>SORRY YOU HAVE NO MORE TRIES ENJOY!</h1>"
+
          }
      }
  }
+
+ function alreadyGuessed(arrUserGuess,key){
+
+     if(arrUserGuess.indexOf(key) < 0 ){
+         document.getElementById("letter-guessed").innerHTML=key_
+     }
+
+ }
+
+
+ /**
+  *
+  * @param arrBlank -  Array of _
+  * @param indexArr - indexes of where the letter was found
+  * @param key      - key that was found
+  * @returns {*}    - new array of blank laters with the key
+  */
+ function replaceArray(arrBlank, indexArr, key){
+     for(var i = 0; i < indexArr.length; i++ ){
+         var keyIndex = indexArr[i]
+         arrBlank[keyIndex] = key
+     }
+     return arrBlank
+ }
+
 
  function findArrayIndexs(key,arrAnswer){
      var index=[]
@@ -84,6 +112,12 @@
      return index
  }
 
+ /**
+  *
+  * @param correctAnswer - correct answer array
+  * @param userInput - from the keyboard
+  * @returns {boolean}
+  */
 
  function checkLetter(correctAnswer, userInput){
      var exist = false
@@ -98,25 +132,43 @@
 
  function generateblankLetters(rand){
      var bandName = bands[rand].name
-     var arrBandName=[]
-     var blank=""
+     var arrBlank=[]
+     var blank="_"
+
      var letters=document.createElement("div")
          letters.setAttribute("id","hint-letters")
      for(var i=0; i<bandName.length;i++){
-         blank= blank +"_ "
-         arrBandName.push(bandName[i])
+         if(!bandName[i] == " ") {
+             blank += "_"
+             arrBlank.push("_")
+         }
      }
      document.getElementById("guess-letter").innerHTML=blank
-     return arrBandName
+     console.log(arrBlank)
+     return arrBlank
  }
+
+
+ function generateWinningArray (rand) {
+     var bandName = bands[rand].name
+     var winArray=[]
+     for(var i=0; i<bandName.length;i++){
+         if(!bandName[i] == " "){
+             winArray.push(bandName[i])
+         }
+     }
+     return winArray
+ }
+
 
  function initGame(){
      var rand = Math.floor(Math.random() * 3)
      document.getElementById("instructions").innerHTML = "<h1>Press any key to Play the best game ever</h1>"
      document.onkeyup = function(evt) {
          if(evt.key){
-             arrAnswer = generateblankLetters(rand)  //return
-             startGame(rand , arrAnswer)
+             arrBlank = generateblankLetters(rand)
+             winArray = generateWinningArray(rand)
+             startGame(rand , winArray,arrBlank)
          }
      }
  }
